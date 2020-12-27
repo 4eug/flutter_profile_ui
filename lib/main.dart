47f88671d0a9af43f27d3ed6f402f779dd25a651
4eug <debraheug@gmail.com>
2020-12-27 12:40:25 +0000

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:profile_screen/Screen/profile_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfileScreen(),
+    );
+  }
+}
